8e6aa24605f88426a46be1c4261e35f5e4ac274f
Socrates <josesocrateslopez@gmail.com>
2020-09-24 16:19:35 -0500

    nuevas funciones

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ let server = http.createServer(appexpress)
 let socket = require('./socket/socket-app')
 const path = require('path')
 const fs = require('fs')
-
 const iconPath = path.join(__dirname, 'logo.png');
+
+//server.maxConnections = 1
+//server.setMaxListeners = 2
+
 socket.initServer(server)
 
 var host = ip.address()
@@ -44,12 +47,31 @@ fs.readFile('configServer.json',function (err, data){
         })
         server.listen(appexpress.get('port'), appexpress.get('host'), () => {
             console.log(`Server listening on ${host}:${port}`)
+            //console.log("getMaxListeners -> ",server.getMaxListeners())
+        })
+
+        appexpress.post("/requestConnection", function(res, req){
+            let status = {
+                message: "server ready"
+            }
+            if(socket.clientConnected() == false){                
+                //server disponible - Acceso concedido
+                req.status(200).send(status)
+            }else{
+                //server lleno - Acceso denegado
+                console.log("requestConnection -> ", res.body, "-> ",socket.getTimeNow())
+                console.log("Server full - faild request -> ",socket.getTimeNow())
+                status.message = "server complete"
+                req.status(200).send(status)
+            }
         })
 
         socket.listenInConnect()
         socket.listenInConnection()
     }
 })
+
+
 
 
 
@@ -65,10 +87,53 @@ ipcMain.on("setPort", (event,newport) => {
     port = newport
 });
 
+ipcMain.on("desconectClient", (event) =>{
+    socket.desconectedClient()
+    removeDevice()
+})
+
+ipcMain.on("fixConnection", (event)=>{
+    let data = {
+        system: global._system,
+        device: global._device,
+        uuid: global._uuid
+    }
+    fs.writeFile('fixDevice.json', JSON.stringify(data), function(err){
+        if(err){
+            console.log(err)
+        }
+    })
+})
+
+ipcMain.on("removefixConnection", (event)=>{
+    deleteDeviceFixed()
+})
+
+function deleteDeviceFixed(){
+    let data = {
+        system: '',
+        device: '',
+        uuid: ''
+    }
+    fs.writeFile('fixDevice.json', JSON.stringify(data), function(err){
+        if(err){
+            console.log(err)
+        }
+    })
+}
+
 ipcMain.on("resetSocketServer", (event) => {  
     writeFileServerConfiguration()  
     resetSocketServer()        
 });
+
+function removeDevice(){
+    global._system = null
+    global._device = null
+    if(global._fix == true){
+        deleteDeviceFixed()
+    }
+}
 
 function writeFileServerConfiguration(){
     let data = {
@@ -104,7 +169,7 @@ app.on('ready', () => {
         webPreferences: {
             nodeIntegration: true,
             enableRemoteModule: true,
-            //devTools: true
+            
         },
         transparent: true,
         frame: false,
@@ -114,7 +179,7 @@ app.on('ready', () => {
     win.loadFile('./src/index.html')
     win.removeMenu()
     win.setIcon(nativeImage.createFromPath(iconPath))
-    
+    win.webContents.openDevTools()
     let tray = new Tray(iconPath)
 
     const ctx = Menu.buildFromTemplate([
@@ -132,7 +197,6 @@ app.on('ready', () => {
                 resetSocketServer()
                 app.relaunch()
                 app.quit()
-
             }
         },
         { type: 'separator' },
--- socket/socket-app.js
+++ socket/socket-app.js
@@ -7,7 +7,8 @@ const directories = require("../api-win/directories-win")
 const shorcutsMedia = require("../api-win/shortcuts-media-win");
 const session = require("../api-win/session-win")
 const os = require('os')
-const os_utils = require('os-utils');    
+const fs = require('fs')
+const os_utils = require('os-utils');
 let io = null
 //Monitores
 let checkConnected = null
@@ -15,9 +16,9 @@ let updateData = null
 let cpuMonitor = null
 function initServer(server) {
     io = socket(server)
-    if(io){
+    if (io) {
         console.log("socket inicializado")
-    }else{
+    } else {
         console.log("socket no inicializado")
     }
     checkConnected = setInterval(() => {
@@ -28,10 +29,10 @@ function initServer(server) {
     }, 1000)
     updateData = setInterval(() => {
         dataInit()
-    }, (60000 * 5))    
+    }, (60000 * 5))
 }
 
-function deleteSocketServer(){
+function deleteSocketServer() {
     clearInterval(checkConnected)
     clearInterval(updateData)
     clearInterval(cpuMonitor)
@@ -39,24 +40,25 @@ function deleteSocketServer(){
 }
 
 function listenInConnect() {
-    io.on('connect', () => {
-        console.log('cliente conectado')
+    io.on('connect', (socket) => {
+        console.log('cliente conectado ',socket.id)
+        //console.log('client -> ',socket)
         dataInit()
 
     })
 }
-function listenMouseModule(socket){
+function listenMouseModule(socket) {
     socket.on("movemouse", coords => {
         mouse.moveMouse(coords)
     });
-    
+
     socket.on("clickLeft", () => {
         mouse.clickLeft()
     })
     socket.on("clickRight", () => {
         mouse.clickRight()
     })
-    
+
     socket.on("setSpeedMouse", (speed) => {
         mouse.setSpeedMouse(speed)
     })
@@ -70,7 +72,7 @@ function listenMouseModule(socket){
         mouse.moveScrollUp()
     })
 }
-function listenDirectoryModule(socket){
+function listenDirectoryModule(socket) {
     socket.on("getDisks", () => {
         let wait = Promise.resolve(directories.getDiskInfo())
         wait.then((data) => {
@@ -93,7 +95,7 @@ function listenDirectoryModule(socket){
     socket.on("executeFile", (dir) => {
         let wait = Promise.resolve(directories.executeFile(dir))
         wait.then((res) => {
-            socket.emit("onExecuteFile", res)            
+            socket.emit("onExecuteFile", res)
             console.log("onExecuteFile -> OK")
         }).catch((error) => {
             socket.emit("onExecuteFile", "fail")
@@ -101,7 +103,7 @@ function listenDirectoryModule(socket){
         })
     })
 }
-function listenMediaModule(socket){
+function listenMediaModule(socket) {
     socket.on("setVolume", vol => {
         volume.setVolume(vol)
     })
@@ -120,7 +122,7 @@ function listenMediaModule(socket){
         shorcutsMedia.next()
     })
 }
-function listenSesionModule(socket){
+function listenSesionModule(socket) {
     socket.on("turnOffPC", () => {
         session.turnOff().then((data) => {
             socket.emit("onStatusSession", data)
@@ -151,7 +153,7 @@ function listenSesionModule(socket){
         })
     })
 
-    cpuMonitor = setInterval(() => {  
+    cpuMonitor = setInterval(() => {
         /*  
         os_utils.cpuFree(function(f){
             let free = parseInt(f*100)
@@ -161,25 +163,60 @@ function listenSesionModule(socket){
             }
             console.log(cpu)
         })
-        */        
-        os_utils.cpuUsage(function(v){        
-            let mdis = os_utils.freememPercentage()*100
-            let mUsed = 100-mdis
+        */
+        os_utils.cpuUsage(function (v) {
+            let mdis = os_utils.freememPercentage() * 100
+            let mUsed = 100 - mdis
             let infoPc = {
-                cpu: Math.round((v*100)) ,
+                cpu: Math.round((v * 100)),
                 memory: parseInt(mUsed),
                 pc: os.hostname()
             }
-            socket.emit("onInfoPC", infoPc)        
+            socket.emit("onInfoPC", infoPc)
         });
-    },1000);
+    }, 1000);
 }
 function listenInConnection() {
     io.on("connection", socket => {
-        socket.on("onDevice", (data) => {
-            console.log("onDevice: ", data)
-            global._system = data.system
-            global._device = data.device
+        socket.on("onDevice", (device) => {
+            console.log("onDevice: ", device)
+            fs.readFile('fixDevice.json',function (err, data){
+                if(err){
+                    console.log(err)
+                }else{
+                    //Si hay conexion fija
+                    if(data.isNull() == false){
+                        let dData = JSON.parse(data)
+                        //Si los datos del dispositivo a conectar son iguales
+                        //al dispositivo fijado se realiza la conexion
+                        //console.log("load -> ", dData)
+                        //console.log("request -> ",device)
+                        if(dData['uuid'] == device.uuid){
+                            global._system = device.system
+                            global._device = device.device
+                            global._uuid = device.uuid   
+                            global._fix = true
+                            console.log("Hay connexión fija") 
+                        }else{                            
+                        //Si es un dispositivo desconocido se niega la conexion
+                        //    socket.to(socket.id).emit("desconectDevice")
+                        // console.log("Intento de conexión denegada -> ",device.uuid) 
+                            if(dData['uuid'].length != 0){
+                                console.log("conexion denegada -> [",dData['uuid'],"] -> ", dData['uuid'].length)
+                                socket.emit("desconectDevice")
+                            }else{
+                                console.log("No hay conexion dija")
+                                global._system = device.system
+                                global._device = device.device
+                                global._uuid = device.uuid
+                                global._fix = false
+                            }
+                        }
+                    }else{
+                        console.log("fix null")
+                    }
+                }
+            })
         })
 
         socket.on("setBrightness", (value) => {
@@ -195,13 +232,16 @@ function listenInConnection() {
         listenDirectoryModule(socket)
         listenMediaModule(socket)
         listenSesionModule(socket)
-        
+
     });
 
 }
+function desconectedClient(){
+    io.emit("desconectDevice")
+}
 function dataInit() {
-    if (clientConnected()) {        
-        console.log("update data -> ",getTimeNow())
+    if (clientConnected()) {
+        console.log("update data -> ", getTimeNow())
         volume.getVolume().then((vol) => {
             io.emit("onVolume", vol)
         })
@@ -219,11 +259,13 @@ function dataInit() {
             }
         })
         sendUserName()
-        console.log("status-connection -> conected "+getTimeNow())
-    }else{
-        console.log("status-connection -> waiting "+getTimeNow())
+        console.log("status-connection -> conected " + getTimeNow())
+    } else {
+        console.log("status-connection -> waiting " + getTimeNow())
     }
 }
+
+
 function sendUserName() {
     let username = os.userInfo().username
     io.emit("onUserName", username)
@@ -232,20 +274,20 @@ function sendUserName() {
 function clientConnected() {
     let clients = null
     try {
-        clients = io.engine.clientsCount    
+        clients = io.engine.clientsCount
         if (clients != 0) {
             return true
         }
     } catch (error) {
         return false
-    }   
+    }
     return false
 }
 
 
-function getTimeNow(){
+function getTimeNow() {
     let date = new Date()
-    let time = "["+date.getHours() +":"+date.getMinutes()+":"+date.getSeconds()+"]"
+    let time = "[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "]"
     return time
 }
-module.exports = { initServer, listenInConnect, listenInConnection,deleteSocketServer }
+module.exports = { initServer, listenInConnect, listenInConnection, deleteSocketServer, clientConnected, getTimeNow, desconectedClient}
